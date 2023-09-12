08ddefee88b5f752535c0c13a8a2bc0e979ba6ad
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2023-09-12 11:25:20 -0300

    add: prisma, docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import { fastify } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    email: 'sasuke@gmail.com',
+    name: 'sasuke',
+  },
+})
 
 export const app = fastify()
