4de37d1f6b0f39bb9054cd4f28f66e8b7c05e3ef
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2023-11-22 11:47:10 -0300

    add : prisma connection for environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,39 @@
+import { randomUUID } from 'node:crypto'
+import 'dotenv/config'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a database URL')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   transformMode: 'ssr',
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    // setup são as config rodadas antes de cada suite de testes
+
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DELETE SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
