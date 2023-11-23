01cf0acc9d582a18b0096485da9ebfd3db5c5fd6
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2023-11-22 21:25:59 -0300

    mod : routes separation ; add : create gym

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,35 @@
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function createGym(request: FastifyRequest, reply: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+      // a latitude sempre tem quer ser <= 90
+      // mas ela pode ser negativa ou positiva
+      // então transformamos o valor pra positivo só pra fazer a validação usando Math.abs
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { title, description, phone, latitude, longitude } =
+    createGymBodySchema.parse(request.body)
+
+  const createGymUseCase = makeCreateGymUseCase()
+
+  await createGymUseCase.run({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  })
+
+  return reply.status(201).send('Gym created')
+}
